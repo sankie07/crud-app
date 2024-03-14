f742c3dee4c5eb11d8ef69d02803790ee6852eb3
sankie <sankie07sc@gmail.com>
2024-03-15 04:52:38 +0530

    dep

--- crud-app-server/app.js
+++ crud-app-server/app.js
@@ -4,21 +4,9 @@ const cors = require("cors");
 require("dotenv/config");
 const app = express();
 
-app.use(cors({
-  origin: ['https://crud-app-pink-sigma.vercel.app"],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
-
-app.use(express.json());
-
-const db = require('./db');
-
-const createCrudRoute = require('./routes/create_crud');
-
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/api', createCrudRoute);
+app.use(bodyparser.json());
+app.use(cors())
+app.use('/api',createCrudRoute)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
