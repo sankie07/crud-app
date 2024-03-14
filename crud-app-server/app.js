760dd30aef83039a1a185127837bd34f751f6221
sankie07 <87243112+sankie07@users.noreply.github.com>
2024-03-15 04:39:19 +0530

    Update app.js

--- crud-app-server/app.js
+++ crud-app-server/app.js
@@ -1,19 +1,26 @@
-const express = require("express")
-const bodyparser = require ("body-parser")
-const cors= require ("cors")
-require ('dotenv/config')
-
-const db=require('./db')
-
-const createCrudRoute = require('./routes/create_crud')
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
 const app = express();
 
-app.use(bodyparser.json());
-app.use(cors())
-app.use('/api',createCrudRoute)
+app.use(cors({
+  origin: ["https://deploy-mern-1wha.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
+app.use(express.json());
+
+const db = require('./db');
+
+const createCrudRoute = require('./routes/create_crud');
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', createCrudRoute);
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Port is running on -http:\\localhost:${port}`);
-})
+});
