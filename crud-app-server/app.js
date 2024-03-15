0a16a5e7b7f11789014ef5c77b627dfae01183aa
sankie07 <87243112+sankie07@users.noreply.github.com>
2024-03-15 05:32:58 +0530

    Update app.js

--- crud-app-server/app.js
+++ crud-app-server/app.js
@@ -4,9 +4,21 @@ const cors = require("cors");
 require("dotenv/config");
 const app = express();
 
-app.use(bodyparser.json());
-app.use(cors())
-app.use('/api',createCrudRoute)
+app.use(cors({
+  origin: ["https://deploy-mern-1wha.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
+app.use(express.json());
+
+const db = require('./db');
+
+const createCrudRoute = require('./routes/create_crud');
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', createCrudRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
