791ceb72886511e449bc057edd180d65c9413022
sankie07 <87243112+sankie07@users.noreply.github.com>
2024-03-15 06:22:34 +0530

    Update db.js

--- crud-app-server/db.js
+++ crud-app-server/db.js
@@ -3,9 +3,10 @@ require('dotenv/config');
 
 const urln = 'mongodb+srv://man:man@cluster0.tycpere.mongodb.net/cred_app?retryWrites=true&w' +
         '=majority&appName=Cluster0'
+const upan= 'https://crud-app-pink-sigma.vercel.app';
 
 mongoose
-    .connect(urln, {
+    .connect(upan, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
